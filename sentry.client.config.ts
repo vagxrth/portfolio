7060aab0b59e59a38cacc400815183710698466f
Vagarth <vagarth419@gmail.com>
2024-06-04 05:53:03 +0530

    bug: fix sending error and performace data to sentry

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -28,7 +28,7 @@ Sentry.init({
     }),
     Sentry.feedbackIntegration({
       // Additional SDK configuration goes in here, for example:
-      colorScheme: "system",
+      colorScheme: "dark",
     }),
   ],
 });
